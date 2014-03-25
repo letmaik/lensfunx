c78eb11eee4112ed714c6b027b761b383cd5ef21
Maik Riechert <maik.riechert@arcor.de>
2014-03-25 17:25:51 +0100

    new plots using model parameters and derivatives

--- plots.py
+++ plots.py
@@ -1,5 +1,6 @@
 from __future__ import division
 
+from functools import partial
 import os
 import numpy as np
 import numpy.ma as ma
@@ -9,6 +10,24 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 from prettyplotlib.colors import blue_red
+
+def ptlens(ru, a, b, c, order=0):
+    if order == 0:
+        return ru*(a*ru**3 + b*ru**2 + c*ru + 1 - a - b- c)
+    else:
+        return 3*a*ru**2 + 2*b*ru + c
+
+def poly3(ru, k1, order=0):
+    if order == 0:
+        return ru*(1 - k1 + k1*ru**2)
+    else:
+        return 2*k1*ru
+
+def poly5(ru, k1, k2, order=0):
+    if order == 0:
+        return ru*(1 + k1*ru**2 + k2*ru**4)
+    elif order == 1:
+        return 2*k1*ru + 4*k2*ru**3
 
 def vectorLengths(vectors):
     return np.sqrt((vectors*vectors).sum(axis=1))
@@ -57,11 +76,21 @@ def lensDistortionRelativeDistance(mod):
 
     return distanceRelative
 
-def drawHeatmap(data, path):
+def drawHeatmap(path, data):
     data = ma.masked_invalid(data, copy=False)
     fig, ax = plt.subplots()
     im = ax.imshow(data, cmap=blue_red)
     fig.colorbar(im)
+    fig.savefig(path)
+    plt.close(fig)
+    
+def drawLinePlot(path, x, y, xlabel=None, ylabel=None):
+    fig,ax = plt.subplots()
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    ax.plot(x, y)
     fig.savefig(path)
     plt.close(fig)
     
@@ -72,13 +101,14 @@ lensModel = 'Nikkor 28mm f/2.8D AF'
 
 db = lensfun.Database()
 cam = db.findCameras(camMaker, camModel)[0]
-lens = db.findLenses(cam, lensMaker, lensModel)[0]
+#lens = db.findLenses(cam, lensMaker, lensModel)[0]
+lens = filter(lambda l: l.Maker == 'Contax', db.getLenses())[0]
 
 distance = 10
 focalLength = lens.MinFocal
 aperture = lens.MinAperture
 width, height = 600, 400 # 3:2 aspect ratio
-
+    
 plotsPath = 'plots'
 if not os.path.exists(plotsPath):
     os.mkdir(plotsPath)
@@ -87,7 +117,34 @@ mod = lensfun.Modifier(lens, cam.CropFactor, width, height)
 mod.initialize(focalLength, aperture, distance)
 
 dist = lensDistortionDistance(mod)
-drawHeatmap(dist, os.path.join(plotsPath, 'dist.svg'))
+drawHeatmap(os.path.join(plotsPath, 'dist.svg'), dist)
 
 distRel = lensDistortionRelativeDistance(mod)
-drawHeatmap(distRel, os.path.join(plotsPath, 'dist_rel.svg'))
+drawHeatmap(os.path.join(plotsPath, 'dist_rel.svg'), distRel)
+
+
+# get the internal models interpolated for the given focal length
+calib = lens.interpolateDistortion(focalLength)
+if calib.Model == lensfun.DistortionModel.PTLENS:
+    a, b, c = calib.Terms
+    rd = partial(ptlens, a=a, b=b, c=c, order=0)
+    rd1 = partial(ptlens, a=a, b=b, c=c, order=1)    
+    
+elif calib.Model == lensfun.DistortionModel.POLY3:
+    k1, = calib.Terms
+    rd = partial(poly3, k1=k1, order=0)
+    rd1 = partial(poly3, k1=k1, order=1)
+    
+elif calib.Model == lensfun.DistortionModel.POLY5:
+    k1, k2, = calib.Terms
+    rd = partial(poly5, k1=k1, k2=k2, order=0)
+    rd1 = partial(poly5, k1=k1, k2=k2, order=1)
+    
+else:
+    raise NotImplementedError
+
+X = np.linspace(0, focalLength/10, 50)
+
+drawLinePlot(os.path.join(plotsPath, 'dist_rel_2.svg'), X, [(rd(x)-x)/x*focalLength for x in X], ylabel='$\mathrm{mm}')
+drawLinePlot(os.path.join(plotsPath, 'deriv.svg'), X, [rd1(x)*focalLength for x in X], ylabel='$\mathrm{mm}^{-1}$')
+
